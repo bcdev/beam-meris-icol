af0319dfdf872faa02a83a1dd0130299409f88e5
marpet <marco.peters@brockmann-consult.de>
2013-01-14 13:21:46 +0100

    added override annotations

--- src/main/java/org/esa/beam/meris/icol/IcolConvolutionJaiConvolve.java
+++ src/main/java/org/esa/beam/meris/icol/IcolConvolutionJaiConvolve.java
@@ -21,21 +21,23 @@ import java.util.Map;
  * @version $Revision: 8078 $ $Date: 2010-01-22 17:24:28 +0100 (Fr, 22 Jan 2010) $
  */
 public class IcolConvolutionJaiConvolve implements IcolConvolutionAlgo {
+
     private final Product convolveSourceProduct;
     private final String namePrefix;
     private int numBands;
     private int[] bandsToSkip;
 
-    public IcolConvolutionJaiConvolve(Product l1bProduct, String productType, CoeffW coeffW, String namePrefix, int iaerConv,
+    public IcolConvolutionJaiConvolve(Product l1bProduct, String productType, CoeffW coeffW, String namePrefix,
+                                      int iaerConv,
                                       int numBands, int[] bandsToSkip) {
         KernelJAI convolveKernel;
         double reshapedScalingFactor;
         if (productType.contains("_RR")) {
             convolveKernel = coeffW.getReshapedConvolutionKernelForRR(iaerConv);
-            reshapedScalingFactor = 2.0 * (CoeffW.RR_KERNEL_SIZE ) / (convolveKernel.getWidth() - 1);
+            reshapedScalingFactor = 2.0 * (CoeffW.RR_KERNEL_SIZE) / (convolveKernel.getWidth() - 1);
         } else {
             convolveKernel = coeffW.getReshapedConvolutionKernelForFR(iaerConv);
-            reshapedScalingFactor = 2.0 * (CoeffW.FR_KERNEL_SIZE ) / (convolveKernel.getWidth() - 1);
+            reshapedScalingFactor = 2.0 * (CoeffW.FR_KERNEL_SIZE) / (convolveKernel.getWidth() - 1);
         }
 
 
@@ -47,13 +49,16 @@ public class IcolConvolutionJaiConvolve implements IcolConvolutionAlgo {
         convolveParams.put("namePrefix", namePrefix);
         convolveParams.put("correctionMode", coeffW.getCorrectionMode());
         convolveParams.put("reshapedScalingFactor", reshapedScalingFactor);
-        convolveSourceProduct = GPF.createProduct(OperatorSpi.getOperatorAlias(ReshapedConvolutionOp.class), convolveParams, l1bProduct);
+        convolveSourceProduct = GPF.createProduct(OperatorSpi.getOperatorAlias(ReshapedConvolutionOp.class),
+                                                  convolveParams, l1bProduct);
     }
 
+    @Override
     public Rectangle mapTargetRect(Rectangle targetRect) {
         return targetRect;
     }
 
+    @Override
     public Convolver createConvolver(Operator op, Tile[] rhoTiles, Rectangle targetRect, ProgressMonitor pm) {
         Tile[] sourceTiles = new Tile[numBands];
         for (int i = 0; i < numBands; i++) {
@@ -62,18 +67,21 @@ public class IcolConvolutionJaiConvolve implements IcolConvolutionAlgo {
             }
             // todo: this only handles band names of the form <name>_n - make more general
             final Band sourceBand = convolveSourceProduct.getBand(namePrefix + (i + 1));
-            sourceTiles[i] = op.getSourceTile(sourceBand, targetRect, BorderExtender.createInstance(BorderExtender.BORDER_COPY));
+            sourceTiles[i] = op.getSourceTile(sourceBand, targetRect,
+                                              BorderExtender.createInstance(BorderExtender.BORDER_COPY));
         }
         return new ConvolverImpl(sourceTiles);
     }
 
     public class ConvolverImpl implements Convolver {
+
         private final Tile[] rhoBracket;
 
         public ConvolverImpl(Tile[] rhoBracket) {
             this.rhoBracket = rhoBracket;
         }
 
+        @Override
         public double[] convolvePixel(int x, int y, int iaer) {
             final double[] pixel = new double[rhoBracket.length];
             for (int b = 0; b < pixel.length; b++) {
@@ -85,6 +93,7 @@ public class IcolConvolutionJaiConvolve implements IcolConvolutionAlgo {
             return pixel;
         }
 
+        @Override
         public double convolveSample(int x, int y, int iaer, int b) {
             return rhoBracket[b].getSampleDouble(x, y);
         }
